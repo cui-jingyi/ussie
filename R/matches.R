b4e2408eb133a96c8c625bd6382908ee8d9f0c79
Jingyi Cui <jingyicui@protonmail.com>
2022-07-25 11:47:51 -0400

    Add helper pages for both the package and the matches function.

--- R/matches.R
+++ R/matches.R
@@ -1,9 +1,24 @@
-uss_make_matches <- function(data_engsoc, country_name) {
+#' Make a standard league-play tibble
+#'
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised column-names and types, e.g. `date` is a `Date`.
+#'
+#' @param data_engsoc  obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'
+#' @return A tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(engsoccerdata::spain, "Spain")
+#' uss_make_matches(engsoccerdata::england, "England")
+uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
     dplyr::transmute(
-      country = as.character(country_name),
+      country = as.character(country),
       tier = factor(tier, levels = c("1", "2", "3", "4")),
       season = as.integer(Season),
       date = as.Date(Date),
